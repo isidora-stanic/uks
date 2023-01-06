37805f8591c56e6923c179f7635d2b7fc017365e
rajtarovN <68690942+rajtarovN@users.noreply.github.com>
2023-01-06 10:24:06 +0100

    changed login function

--- mini_githubcic/mini_githubcic/views.py
+++ mini_githubcic/mini_githubcic/views.py
@@ -14,41 +14,24 @@ def index(request):
     return render(request,'index.html',{"title":title})
 
 def login(request,id=None):
-    #title = apps.get_app_config('login').verbose_name
     if request.method == 'GET':
         return render(request, "login.html")
     if request.method=='POST':
-        users=User.objects.all()
 
         username = request.POST['username']
         password = request.POST['password']
-        user = User.objects.get(username=username)
-        if user is None:
-            return render(request, "login.html",
-                         { "users_error": "User with this username and password does not exist"}) #"title": title,
 
-        if user.password != password:
-            return render(request, "login.html",
-                          { "users_error": "User with this username and password does not exist"}) #,"title": title,
+        try:
+            user = User.objects.get(username=username)
+            if user.password != password:
+                return render(request, "login.html",
+                              {"users_error": "User with this username and password does not exist"})
 
-        else:
             return render(request, "index.html", {"title": "first page"})
 
-# class LoginView(CreateView):
-#     model = Project
-#     template_name = 'login.html'
-#     context_object_name = 'login'
-#     ordering = ['title']
-#
-#     def form_valid(self, form):
-#         # TODO link to logged in user
-#         form.instance.lead = User.objects.get(username="U1")
-#         form.instance.link = "https://github.com/" + form.instance.lead.username + "/" + form.instance.title + ".git"
-#         if Project.objects.filter(title=form.instance.title).exists():
-#             form.add_error('titleExists', 'Title already in use')
-#
-#         return super().form_valid(form)
-
+        except:
+            return render(request, "login.html",
+                {"users_error": "User with this username and password does not exist"})
 
 def index(request):
     title = apps.get_app_config('mini_githubcic').verbose_name
