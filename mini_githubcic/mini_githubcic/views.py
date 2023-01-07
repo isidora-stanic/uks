1a640cd018ddd469390aa5bb398835aa9983a6f3
isidora-stanic <isidora.stanic@outlook.com>
2023-01-07 19:37:13 +0100

    Implemented CRUD operations for Issues, closing and opening issues and filtering them by that property

--- mini_githubcic/mini_githubcic/views.py
+++ mini_githubcic/mini_githubcic/views.py
@@ -1,5 +1,6 @@
 from django.apps.registry import apps
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 from django.views.generic import (
     CreateView,
     ListView,
@@ -7,7 +8,7 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from .models import Project, User
+from .models import Project, User, Issue
 
 
 def index(request):
@@ -36,11 +37,6 @@ def login(request, id=None):
                           {"users_error": "User with this username and password does not exist"})
 
 
-def index(request):
-    title = apps.get_app_config('mini_githubcic').verbose_name
-    return render(request, 'index.html', {"title": title})
-
-
 class ProjectListView(ListView):
     model = Project
     template_name = 'list_projects.html'
@@ -49,6 +45,20 @@ class ProjectListView(ListView):
 
     def get_queryset(self):
         return Project.objects.all()
+
+
+class IssueListView(ListView):
+    model = Issue
+    template_name = 'list_issues.html'
+    context_object_name = 'issues'
+    ordering = ['id']
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(IssueListView, self).get_context_data(*args, **kwargs)
+        context['project_id'] = self.request.resolver_match.kwargs['pk']
+        context['project'] = Project.objects.filter(id=context['project_id']).first()
+        context['issues'] = Issue.objects.filter(project__id=context['project_id'])
+        return context
 
 
 class ProjectCreateView(CreateView):
@@ -66,6 +76,28 @@ class ProjectCreateView(CreateView):
         return super().form_valid(form)
 
 
+class IssueCreateView(CreateView):
+    model = Issue
+    template_name = 'new_issue.html'
+    fields = ['title', 'description', 'assigned_to']
+
+    def form_valid(self, form):
+        if Issue.objects.filter(title=form.instance.title).exists():
+            form.add_error('titleExists', 'Title already in use')
+        context = self.get_context_data()
+        form.instance.project = context['project']
+        form.instance.creator = User.objects.get(username="U1")
+        # todo treba ulogovani korisnik self.request.user
+        form.instance.date_created = timezone.now()
+        return super().form_valid(form)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(IssueCreateView, self).get_context_data(*args, **kwargs)
+        context['project_id'] = self.request.resolver_match.kwargs['pk']
+        context['project'] = Project.objects.filter(id=int(context['project_id'])).first()
+        return context
+
+
 class ProjectUpdateView(UpdateView):
     model = Project
     template_name = 'project_update.html'
@@ -80,9 +112,28 @@ class ProjectUpdateView(UpdateView):
         return super().form_valid(form)
 
 
+class IssueUpdateView(UpdateView):
+    model = Issue
+    template_name = 'issue_update.html'
+    fields = ['title', 'description', 'assigned_to', 'is_open']
+
+    def form_valid(self, form):
+
+        if Issue.objects.filter(title=form.instance.title).exists():
+            if self.get_object().id != form.instance.id:
+                form.add_error('titleExists', 'Title already in use')
+
+        return super().form_valid(form)
+
+
 class ProjectDetailView(DetailView):
     model = Project
     template_name = 'project_detail.html'
+
+
+class IssueDetailView(DetailView):
+    model = Issue
+    template_name = 'issue_detail.html'
 
 
 class ProjectDeleteView(DeleteView):
@@ -93,3 +144,24 @@ class ProjectDeleteView(DeleteView):
     def test_func(self):
         # TODO check if request sender is project lead
         return True
+
+
+class IssueDeleteView(DeleteView):
+    model = Issue
+    template_name = 'issue_delete.html'
+    success_url = '../'
+
+    def test_func(self):
+        # TODO check if request sender is developer on the project
+        return True
+
+
+def issue_state_toggle(request, pk=None, ik=None):
+    if request.method == 'GET':
+        issue = Issue.objects.get(id=ik)
+        if issue.is_open:
+            issue.is_open = False
+        else:
+            issue.is_open = True
+        issue.save()
+        return redirect(issue)
